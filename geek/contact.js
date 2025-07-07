dc9cf76d14e3b685924be90cac9d84332774cf5b
Creative-tobi <adeoyeaisha533@gmail.com>
2025-07-07 12:09:10 +0100

    contact

--- geek/contact.js
+++ geek/contact.js
@@ -46,3 +46,42 @@ let cars = car
  )
  .join("")
  document.getElementById("location").innerHTML = cars
+
+ function CharacterCount() {
+
+   let text = document.getElementById("text").value;
+   let zeo = text.length;
+   document.getElementById("zero").textContent = zeo;
+
+ }
+
+
+ var call = document.getElementById("text");
+ call.addEventListener("input", CharacterCount);
+
+ function submit(){
+  let mail = document.getElementById("mail");
+  let email = document.getElementById("email");
+
+  let enter = document.getElementById("enter");
+  let numb = document.getElementById("numb");
+
+
+  if ((mail.value).includes ("@") && (mail.value).includes (".")){
+    email.innerHTML = " "
+  } else{
+    email.innerHTML = "Enter a valid maill"
+    email.style.color = "red"
+  }
+
+
+  if ((enter.value.length < 10) || (enter.value.length > 15) || isNaN(enter.value)){
+    numb.innerHTML = "Invalid phone number"
+    numb.style.color = "red"
+  } else {
+    numb.innerHTML = " ";
+  }
+ }
+
+ var sub = document.getElementById("send");
+ sub.addEventListener("click", submit)
