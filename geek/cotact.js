418feda0437d3a5a3f60f328f160889138d8b8a2
Creative-tobi <adeoyeaisha533@gmail.com>
2025-07-07 14:07:43 +0100

    change

--- geek/cotact.js
+++ geek/cotact.js
@@ -1,10 +1,13 @@
 document.getElementById("demo").innerHTML = "Always here to help you";
-document.getElementById("dem").innerHTML = "Ready to get started?"
-document.getElementById("par").innerHTML = "Your personal informations will not be publish"
-document.getElementById("para").innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem odio
+document.getElementById("dem").innerHTML = "Ready to get started?";
+document.getElementById("par").innerHTML =
+  "Your personal informations will not be publish";
+document.getElementById(
+  "para"
+).innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem odio
           consequuntur maxime neque accusantium! Porro voluptatibus inventore
           libero ad perferendis? Autem vero maxime laudantium sint voluptate,
-          aut a cum nulla.`
+          aut a cum nulla.`;
 
 const car = [
   {
@@ -25,12 +28,13 @@ const car = [
   {
     icon: `<i class="fa-solid fa-clock"></i>`,
     title: `Hours of operation`,
-    desc: `2 hrs/day`
+    desc: `2 hrs/day`,
   },
 ];
 
 let cars = car
- .map((item) => `
+  .map(
+    (item) => `
         <div class="flex py-1">
             <p class="border-2 rounded-full border-stone-200 bg-stone-200 w-10 px-4 py-3 pr-8">${item.icon}</p>
             
@@ -42,46 +46,44 @@ let cars = car
         </div>
  
  `
+  )
+  .join("");
+document.getElementById("location").innerHTML = cars;
 
- )
- .join("")
- document.getElementById("location").innerHTML = cars
+function CharacterCount() {
+  let text = document.getElementById("text").value;
+  let zeo = text.length;
+  document.getElementById("zero").textContent = zeo;
+}
 
- function CharacterCount() {
+var call = document.getElementById("text");
+call.addEventListener("input", CharacterCount);
 
-   let text = document.getElementById("text").value;
-   let zeo = text.length;
-   document.getElementById("zero").textContent = zeo;
-
- }
-
-
- var call = document.getElementById("text");
- call.addEventListener("input", CharacterCount);
-
- function submit(){
+function submit() {
   let mail = document.getElementById("mail");
   let email = document.getElementById("email");
 
   let enter = document.getElementById("enter");
   let numb = document.getElementById("numb");
 
-
-  if ((mail.value).includes ("@") && (mail.value).includes (".")){
-    email.innerHTML = " "
-  } else{
-    email.innerHTML = "Enter a valid maill"
-    email.style.color = "red"
+  if (mail.value.includes("@") && mail.value.includes(".")) {
+    email.innerHTML = " ";
+  } else {
+    email.innerHTML = "Enter a valid maill";
+    email.style.color = "red";
   }
 
-
-  if ((enter.value.length < 10) || (enter.value.length > 15) || isNaN(enter.value)){
-    numb.innerHTML = "Invalid phone number"
-    numb.style.color = "red"
+  if (
+    enter.value.length < 10 ||
+    enter.value.length > 15 ||
+    isNaN(enter.value)
+  ) {
+    numb.innerHTML = "Invalid phone number";
+    numb.style.color = "red";
   } else {
     numb.innerHTML = " ";
   }
- }
+}
 
- var sub = document.getElementById("send");
- sub.addEventListener("click", submit)
+var sub = document.getElementById("send");
+sub.addEventListener("click", submit);
